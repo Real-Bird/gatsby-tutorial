a1d27744685b4b508bbe545d8fd78695ef35ac53
Real-Bird <marbil815@gmail.com>
2022-11-30 17:05:55 +0900

    22-11-30 #10.0~10.7

---
+++ src/pages/blog.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "../components/layout";
+import Seo from "../components/Seo";
+
+const Blog = () => {
+  return (
+    <Layout title="It's my Blog">
+      <p>The most recent news from my shop</p>
+    </Layout>
+  );
+};
+
+export default Blog;
+
+export const Head = () => <Seo title="Blog" />;
